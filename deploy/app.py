f23d260751ee5d2cff83626deb5f7ffc52888632
yufung <4027357+yufung@users.noreply.github.com>
2024-01-07 05:55:28 +0800

    Update deploy files

--- deploy/app.py
+++ deploy/app.py
@@ -1,27 +1,33 @@
 from flask import Flask, request, render_template
-import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import joblib
 
 app = Flask(__name__)
 
-model = joblib.load('./model.joblib')
-vocabulary = joblib.load('./vocabulary.joblib')
-classes = {0: 'Facebook', 1: 'Twitter'}
-cvec = CountVectorizer(max_features = 500, stop_words = 'english', vocabulary = vocabulary)
+mnb_model = joblib.load('./mnb_model.joblib')
+subreddit = {0: '/r/relationship_advice', 1: '/r/personalfinance'}
 
 @app.route('/', methods = ["GET", "POST"])
-def predict():
+def predict_interface():
     output = None
+    proba = None
+    postTitle = None
     postText = None
+
     if request.method == "POST":
+        postTitle = str(request.form['postTitle'])
         postText = str(request.form['postText'])
-        input_cvec = pd.DataFrame(cvec.transform([postText]).todense(), columns = cvec.get_feature_names())
-        output = classes[model.predict(input_cvec)[0]]
+        user_input = postTitle + ' ' + postText
+        output = subreddit[mnb_model.predict([user_input])[0]]
+        mnb_prob = mnb_model.predict_proba([user_input]).tolist()
+        proba_dict = {subreddit[k]: round(mnb_prob[0][i] * 100, 2) for i, k in enumerate(mnb_model.classes_.tolist())}
+        proba = dict(sorted(proba_dict.items(), key=lambda x:x[1], reverse=True))
     return render_template(
       "predict.html",
       output = output,
+      proba = proba,
+      postTitle = postTitle,
       postText = postText
       )
 
